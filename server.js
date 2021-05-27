781b43245d3530f33e2b2b83a5bcc32a32b24009
M <markus.stachl@googlemail.com>
2021-05-27 20:54:50 +0200

    add env port to server.js

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/*', function(req, res) {
 });
 
 // Listen for requests at the PORT specified by env variables or the default Heroku port, which is 8080.
-app.listen(port, () => {
+app.listen(process.env.port || port, () => {
     console.log(`Server listening on the port::${port}`);
 });
