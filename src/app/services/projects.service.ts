f94b751047f7427fd67b84d8a5395b8ce03afbcc
M <markus.stachl@googlemail.com>
2021-08-17 19:18:30 +0200

    replace english with german texts

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -7,12 +7,12 @@ export class ProjectsService {
 
   projectList = [{
     href: '/mandelbrot', 
-    title: 'Generating fractal images from the mandelbrot set', 
-    altText: 'Mandelbrot generator', 
+    title: 'Generierung fraktaler Bilder aus der Mandelbrot-Menge', 
+    altText: 'Mandelbrot Bildgenerator', 
     img: 'assets/img/Mandel_zoom_00_mandelbrot_set.jpg', 
     author: 'Markus Stachl', 
     date: '01.12.2020', 
-    desc: 'A small Javascript web application to generate images from the famous mandelbrot set including zooming, coloring.'
+    desc: 'Eine kleine Javascript-Webanwendung zur Generierung frakaler Bilder aus der bekannten Mandelbrot-Menge.'
   }, {
     href: '/raspi', 
     title: 'Raspberry Pi: Digitaler Bilderrahmen', 
